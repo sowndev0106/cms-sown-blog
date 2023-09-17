365ddcca4c81a95bfbd965173e4631d4ca25727c
NGUYỄN THANH SƠN <sowndev0106@gmail.com>
2023-09-17 16:08:07 +0700

    reomve type

--- dist/src/api/type/controllers/type.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * type controller
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::type.type');
--- src/api/type/controllers/type.ts
+++
@@ -1,7 +0,0 @@
-/**
- * type controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::type.type');
